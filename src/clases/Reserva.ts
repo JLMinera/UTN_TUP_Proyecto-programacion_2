cfe7bfbe8d164ef8fc8c4e13438dd247afdef0c6
Walter Ramos <dianacc.alali@gmail.com>
2025-09-21 08:44:05 -0300

    feature:agregar clase vehiculo

--- src/clases/Reserva.ts
+++ src/clases/Reserva.ts
@@ -1,3 +1,16 @@
-export default class Reserva{
-    
+import Estado from "./Estado";
+import Vehiculo from "./Vehiculo";
+
+export default class Reserva extends Estado{
+    private vehiculo: Vehiculo;
+
+    constructor(vehiculo:Vehiculo, fechaInicio: number, fechaFin:number ){
+        super(fechaInicio, fechaFin)
+        this.vehiculo=vehiculo;
+    }
+
+    public setVehiculo(value:Vehiculo){
+        this.vehiculo=value;
+
+}
 }
