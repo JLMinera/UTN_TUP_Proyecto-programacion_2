f6dcac9b26068ebb998a7089710ec6d33be4749d
minerajl <jminera@fi.uba.ar>
2025-09-23 18:56:17 +0000

    Se modifica clase para incluir atributos vehiculo y cliente

--- src/clases/GestorDeReserva.ts
+++ src/clases/GestorDeReserva.ts
@@ -1,35 +1,72 @@
+import Cliente from "./Cliente"
+import Vehiculo from "./Vehiculo"
+
 export default class GestorDeReserva {
     private kilometrajeInicial: number;
     private kilometrajeFinal: number;
     private distanciaRecorrida: number;
+    private vehiculo: Vehiculo;
+    private cliente: Cliente;
+    private vehiculoDevuelto: boolean;
 
-    constructor(kilometrajeInicial: number, kilometrajeFinal: number, distanciaRecorrida: number){
-        this.kilometrajeInicial = kilometrajeInicial;
-        this.kilometrajeFinal =  kilometrajeFinal;
-        this.distanciaRecorrida = distanciaRecorrida;
+    constructor(vehiculo: Vehiculo, cliente: Cliente){
+        this.cliente = cliente;
+        this.vehiculo = vehiculo;
+        this.kilometrajeInicial = vehiculo.getKilometraje();
+        this.kilometrajeFinal = vehiculo.getKilometraje();
+        this.distanciaRecorrida = this.kilometrajeFinal-this.kilometrajeInicial;
+        this.vehiculoDevuelto = false;
+
     }
 
-    public setKilometrajeInicial(kilometrajeInicial: number){
-        this.kilometrajeInicial = kilometrajeInicial;
+    public setKilometrajeInicial(data: number){
+        this.kilometrajeInicial = data;
     }
 
     public getKilometrajeInicial(): number {
         return this.kilometrajeInicial;
     }
 
-    public setKilometrajeFinal(kilometrajeFinal: number){
-        this.kilometrajeFinal = kilometrajeFinal;
+    public setKilometrajeFinal(data: number){
+        this.kilometrajeFinal = data;
     }
 
     public getKilometrajeFinal(): number {
         return this.kilometrajeFinal;
     }
 
-    public setDistanciaRecorrida(distanciaRecorrida: number){
-        this.distanciaRecorrida = distanciaRecorrida;
+    public setDistanciaRecorrida(data: number){
+        this.distanciaRecorrida = data;
     }
 
     public getDistanciaRecorrida(): number {
         return this.distanciaRecorrida;
+    }
+
+    public getCliente(): Cliente {
+        return this.cliente;
+    }
+
+    public setCliente(data: Cliente) {
+        this.cliente = data;
+    }
+
+    public getVehiculo(): Vehiculo{
+        return this.vehiculo;
+    }
+
+    public setVehiculo(data: Vehiculo) {
+        this.vehiculo = data;
+    }
+
+    public getVehiculoDevuelto(): boolean {
+        return this.vehiculoDevuelto;
+    }
+
+    public setVehiculoDevuelto() {
+        this.vehiculoDevuelto = true;
+
+        this.kilometrajeFinal = this.vehiculo.getKilometraje();
+        this.distanciaRecorrida = this.kilometrajeFinal - this.kilometrajeInicial;
     }
 }
