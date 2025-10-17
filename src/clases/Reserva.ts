6745919fa7708d9b991e83152de44493118f959f
Diana2754 <dianacc.alali@gmail.com>
2025-10-16 22:29:30 -0300

    feature: actualizacion de la clase Estado y clases derivadas

--- src/clases/Reserva.ts
+++ src/clases/Reserva.ts
@@ -1,16 +1,39 @@
 import Estado from "./Estado";
 import Vehiculo from "./Vehiculo";
 
-export default class Reserva extends Estado{
-    private vehiculo: Vehiculo;
+export default class Reserva extends Estado {
+    private vehiculos: Map<string, Vehiculo>;
+    public fechaInicio: Date;
+    public fechaFin: Date;
 
-    constructor(vehiculo:Vehiculo, fechaInicio: number, fechaFin:number ){
-        super(fechaInicio, fechaFin)
-        this.vehiculo=vehiculo;
+    constructor(fechaInicio: Date, fechaFin: Date) {
+        super(fechaInicio, fechaFin);
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.vehiculos = new Map<string, Vehiculo>();
     }
 
-    public setVehiculo(value:Vehiculo){
-        this.vehiculo=value;
+    public getFechaInicio(): Date {
+        return this.fechaInicio;
+    }
 
-}
+    public setFechaInicio(value: Date): void {
+        this.fechaInicio = value;
+    }
+
+    public getFechaFin(): Date {
+        return this.fechaFin;
+    }
+
+    public setFechaFin(value: Date): void {
+        this.fechaFin = value;
+    }
+
+    public agregarVehiculo(patente: string, vehiculo: Vehiculo): void {
+        this.vehiculos.set(patente, vehiculo);
+    }
+
+    public quitarVehiculo(patente: string): void {
+        this.vehiculos.delete(patente);
+    }
 }
