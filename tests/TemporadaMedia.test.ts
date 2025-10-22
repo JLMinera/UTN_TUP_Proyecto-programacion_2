989d8a55007667a73b6a461dede8f79e9a40d9a0
JLMinera <minera.jose.luis@gmail.com>
2025-10-22 03:33:05 +0000

    Se añade test temporadaMedia

---
+++ tests/TemporadaMedia.test.ts
@@ -0,0 +1,64 @@
+import TemporadaMedia from '../src/clases/TemporadaMedia'
+
+describe("Test clase TemporadaMedia", () => {
+    let temporadaMedia: TemporadaMedia;
+    let fechaInicio: Date;
+    let fechaFin: Date;
+
+    beforeEach( () => {
+        fechaInicio = new Date("22-03-2025");
+        fechaFin = new Date("25-05-2025");
+        temporadaMedia = new TemporadaMedia(fechaInicio, fechaFin);
+    });
+
+    afterEach(() => {});
+
+    it("El constructor de la clase debe instanciar un objeto del tipo TemporadaMedia", () => {
+        expect(temporadaMedia).toBeInstanceOf(TemporadaMedia);
+    });
+
+    it("Debe devolver la fecha inicio temporada Media", () => {
+        const fechaInicioTest = new Date("22-03-2025");
+        expect(temporadaMedia.getFechaInicio()).toEqual(fechaInicioTest);
+    });
+
+    it("Debe devolver la fecha fin temporada media", () => {
+        const fechaFinTest = new Date("25-05-2025");
+        expect(temporadaMedia.getFechaFin()).toEqual(fechaFinTest);
+    });
+
+    it("Debe devolver el valor de recargo temporada medap", () => {
+        expect(temporadaMedia.getRecargo()).toEqual(1);
+    });
+
+    it("Debe establecer la fecha inicio temporada media", () => {
+        const fecha = new Date("10-11-2025");
+        temporadaMedia.setFechaInicio(fecha);
+
+        expect(temporadaMedia["fechaInicio"]).toEqual(fecha);
+    });
+
+    it("Debe establecer la fecha fin temporada media", () => {
+        const fecha = new Date("03-12-2025");
+        temporadaMedia.setFechaInicio(fecha);
+
+        expect(temporadaMedia["fechaFin"]).toEqual(fecha);
+    });
+    
+    it("Debe establecer el valor del recargo temporada media", () => {
+        temporadaMedia.setRecargo(1.5);
+        expect(temporadaMedia["recargo"]).toEqual(1.5);
+    });
+
+    it("Debe devolver que fecha está dentro del rango de fechas temporada media", () => {
+        let fechaReserva = new Date("30-11-2025");
+        
+        expect(temporadaMedia.reservaEnTemporada(fechaReserva)).toEqual(true);
+    });
+
+    it("Debe devolver que fecha no está dentro del rango de fechas temporada alta", () => {
+        let fechaReserva = new Date("01-11-2028");
+        
+        expect(temporadaMediareservaEnTemporada(fechaReserva)).toEqual(false);
+    });
+})
