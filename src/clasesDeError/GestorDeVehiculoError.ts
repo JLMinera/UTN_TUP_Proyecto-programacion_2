4e57c9a845948272d9e9fada419a61594166cff1
unknown <BrendaAGomez>
2025-11-09 03:41:10 -0300

    Se creo la clase GestorDeVehiculo con los metodos para el ultimo km y fecha de mantenimiento, el contador, se creo la clase error de GestorDeVehiculo, y los test de GestorDeVehiculo

---
+++ src/clasesDeError/GestorDeVehiculoError.ts
@@ -0,0 +1,11 @@
+export default class GestorDeVehiculoError extends Error {
+
+    constructor(mensaje: string) {
+        super(mensaje);
+        this.name = "GestorDeVehiculoError"
+        Object.setPrototypeOf(this, GestorDeVehiculoError.prototype);
+    }
+
+
+
+}
