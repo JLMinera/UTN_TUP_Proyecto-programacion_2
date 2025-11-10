537ef6ce7fb1bf34eb769928b61435649a24c15f
Minera José Luis <minerajl@debian>
2025-11-09 23:35:05 -0300

    Se corrigen tests NecesitaLimpieza

--- tests/NecesitaLimpieza.test.ts
+++ tests/NecesitaLimpieza.test.ts
@@ -1,13 +1,7 @@
-import Disponible from "../src/clases/Disponible";
+
 import NecesitaLimpieza from "../src/clases/NecesitaLimpieza";
-import Vehiculo from "../src/clases/Vehiculo";
-
-class VehiculoTest extends Vehiculo{}
-
 
 describe("tests de la clase NecesitaLimpieza", () => {
-    let vehiculo1: VehiculoTest;
-    let disponible: Disponible;
     let fechaInicio: Date;
     let fechaFin: Date;
     let limpieza: NecesitaLimpieza;
@@ -15,14 +9,11 @@ describe("tests de la clase NecesitaLimpieza", () => {
     beforeEach(() => {
     fechaInicio = new Date(2025, 10, 16);
     fechaFin = new Date(2025, 10, 17);
-    vehiculo1 = new VehiculoTest("ABC123", 500, 24000);
-    disponible = new Disponible(fechaInicio, fechaFin);
     limpieza = new NecesitaLimpieza(200, fechaInicio, fechaFin);
 })
 
 test("El constructor de la clase debe instanciar un objeto del tipo NecesitaLimpieza", () => {
-    const limpieza2 = new NecesitaLimpieza(222, fechaInicio, fechaFin);
-    expect(limpieza2).toBeInstanceOf(NecesitaLimpieza);
+    expect(limpieza).toBeInstanceOf(NecesitaLimpieza);
 });
 
 test ("Devolver fecha inicio", () => {
