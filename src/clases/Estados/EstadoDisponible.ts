e71e0e3a276ba1abd7c57f93056ded9fcdd5c0ee
JLMinera <minera.jose.luis@gmail.com>
2025-11-13 04:52:23 +0000

    Se modifican estados para contemplar cambio estados Mantenimiento y necesitaLimpieza (se elimina logica disparadorMantenimiento)

--- src/clases/Estados/EstadoDisponible.ts
+++ src/clases/Estados/EstadoDisponible.ts
@@ -1,21 +1,26 @@
 import EstadoError from "../../clasesDeError/EstadoError";
 import EstadoVehiculo from "../../interfaz/EstadoVehiculo";
 import GestorDeVehiculo from "../GestorDeVehiculo";
+import Cliente from "../Personas/Cliente";
 import EstadoReservado from "./EstadoReservado";
 
 
 export default class EstadoDisponible implements EstadoVehiculo {
 
-    enviarDisponible(gestorVehiculo: GestorDeVehiculo): void {
+    public enviarDisponible(gestorVehiculo: GestorDeVehiculo): void {
         throw new EstadoError("Vehiculo ya se encuentra en Disponible");
     }
 
-    enviarReservar(gestorVehiculo: GestorDeVehiculo): void {
-        gestorVehiculo.setEstado(new EstadoReservado());
+    public enviarReservar(gestorVehiculo: GestorDeVehiculo, cliente: Cliente, fechaInicio: Date, fechaFin: Date): void {
+        gestorVehiculo.setEstado(new EstadoReservado(cliente, fechaInicio, fechaFin));
     }
 
-    disparadorMantenimiento(gestorVehiculo: GestorDeVehiculo): void {
-        
+    public enviarMantenimiento(gestorVehiculo: GestorDeVehiculo, costo: number, fechaInicio: Date, fechaFin: Date): void {
+        throw new EstadoError("Vehiculo debe reservarse antes de enviar a mantenimiento");
+    }
+
+    public enviarNecesitaLimpieza(gestorVehiculo: GestorDeVehiculo, distanciaRecorrida: number, fechaInicio: Date, fechaFin: Date): void {
+        throw new EstadoError("Vehiculo debe reservarse antes de enviar a necesita limpieza");
     }
     
 }
--- src/clases/Estados/EstadoReservado.ts
+++ src/clases/Estados/EstadoReservado.ts
@@ -1,32 +1,77 @@
 import EstadoError from "../../clasesDeError/EstadoError";
+import ReservaError from "../../clasesDeError/ReservaError";
 import EstadoVehiculo from "../../interfaz/EstadoVehiculo";
 import GestorDeVehiculo from "../GestorDeVehiculo";
+import Cliente from "../Personas/Cliente";
 import EstadoMantenimiento from "./EstadoMantenimiento";
 import EstadoNecesitaLimpieza from "./EstadoNecesitaLimpieza";
 
 export default class EstadoReservado implements EstadoVehiculo {
 
-    enviarDisponible(gestorVehiculo: GestorDeVehiculo): void {
+    private cliente!: Cliente;
+    private fechaInicio!: Date;
+    private fechaFin!: Date;
+    
+    constructor(cliente: Cliente, fechaInicio: Date, fechaFin: Date) {
+        this.setFechaInicio(fechaInicio);
+        this.setFechaFin(fechaFin);    
+        this.setCliente(cliente);
+    }
+
+
+    public enviarDisponible(gestorVehiculo: GestorDeVehiculo): void {
         throw new EstadoError("Vehiculo debe evaluarse si necesita mantenimiento o limpieza");
     }
 
-    enviarReservar(gestorVehiculo: GestorDeVehiculo): void {
+    public enviarReservar(gestorVehiculo: GestorDeVehiculo, cliente: Cliente, fechaInicio: Date, fechaFin: Date): void {
         throw new EstadoError("Vehiculo ya se encuentra en Reservado");
     }
 
-    disparadorMantenimiento(gestorVehiculo: GestorDeVehiculo): void {
-        const ultimoMantenimientoKm = gestorVehiculo.getKilometrajeActual() - gestorVehiculo.getUltimoKmMantenimiento();
-        const fechaActual = new Date();
-        const meses = (fechaActual.getFullYear() - gestorVehiculo.getFechaUltimoMantenimiento().getFullYear()) * 12
-            + (fechaActual.getMonth() - gestorVehiculo.getFechaUltimoMantenimiento().getMonth());
-        const alquileres = gestorVehiculo.getContadorAcumulado() % 5;
+    public enviarMantenimiento(gestorVehiculo: GestorDeVehiculo, costo: number, fechaInicio: Date, fechaFin: Date): void {
+        gestorVehiculo.setEstado(new EstadoMantenimiento(costo, fechaInicio, fechaFin));
+    }   
+ 
+    public enviarNecesitaLimpieza(gestorVehiculo: GestorDeVehiculo, distanciaRecorrida: number, fechaInicio: Date, fechaFin: Date): void {
+        gestorVehiculo.setEstado(new EstadoNecesitaLimpieza(distanciaRecorrida, fechaInicio, fechaFin));
+    }
 
-        if (ultimoMantenimientoKm > 10000 || meses > 12 || alquileres === 0) {
-            gestorVehiculo.setEstado(new EstadoMantenimiento());
-        }
-        else {
-            gestorVehiculo.setEstado(new EstadoNecesitaLimpieza());
-        }
+    public getCliente(): Cliente {
+        return this.cliente;
     }
     
+    public setCliente(cliente: Cliente): void {
+        if (!cliente) {
+                throw new ReservaError("El cliente no puede ser nulo");
+        }
+        this.cliente = cliente;
+    }
+
+    public setFechaInicio(data: Date): void {
+        if (isNaN(data.getTime())) {
+            throw new EstadoError("La fecha de inicio no es válida");
+        }
+            
+        if (this.fechaFin && data > this.fechaFin) {
+            throw new EstadoError("La fecha de inicio no puede ser posterior a la fecha de fin");
+        }
+        this.fechaInicio = data;
+    }
+    
+    public getFechaInicio(): Date {
+        return this.fechaInicio;
+    }
+    
+    public setFechaFin(data: Date): void {
+        if (isNaN(data.getTime())) {
+            throw new EstadoError("La fecha de fin no es válida");
+        }
+        if (this.fechaInicio && data < this.fechaInicio) {
+            throw new EstadoError("La fecha de fin no puede ser anterior a la fecha de inicio");
+        }
+        this.fechaFin = data;
+    }
+    
+    public getFechaFin(): Date {
+            return this.fechaFin;
+    }
 }
