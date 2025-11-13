27a56fdea96aa518bb49c96254c53f565e1df730
JLMinera <minera.jose.luis@gmail.com>
2025-11-13 04:50:10 +0000

    Se actualiza interfaz con agregado cambio estado enviarMentenimiento y enviarNecesitaLimpieza

--- src/interfaz/EstadoVehiculo.ts
+++ src/interfaz/EstadoVehiculo.ts
@@ -1,7 +1,9 @@
 import GestorVehiculo from "../clases/GestorDeVehiculo"
+import Cliente from "../clases/Personas/Cliente";
 
 export default interface EstadoVehiculo {
     enviarDisponible(gestorVehiculo: GestorVehiculo): void;
-    enviarReservar(gestorVehiculo: GestorVehiculo): void;
-    disparadorMantenimiento(gestorVehiculo: GestorVehiculo): void;
+    enviarReservar(gestorVehiculo: GestorVehiculo, cliente: Cliente, fechaInicio: Date, fechaFin: Date): void;
+    enviarMantenimiento(gestorVehiculo: GestorVehiculo, costo: number, fechaInicio: Date, fechaFin: Date): void;
+    enviarNecesitaLimpieza(gestorVehiculo: GestorVehiculo, distanciaRecorrida: number, fechaInicio: Date, fechaFin: Date): void;
 }
